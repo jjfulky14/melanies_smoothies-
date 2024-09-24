e79bba5735377bc65314722b285826e329daaac1
jjfulky14 <jfulkerson@spauldingridge.com>
2024-09-23 18:31:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ import re
 import pandas as pd
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to five ingredients:'
